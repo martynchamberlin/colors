b6dd6dc0917b9372cb8696689de65604913088ef
Martyn Chamberlin <mail@martynchamberlin.com>
2014-02-26 01:25:43 -0600

    really fighting the syntax

--- PCLViewController.m
+++ PCLViewController.m
@@ -24,6 +24,11 @@
 @property (nonatomic, weak) IBOutlet UITextField *hexInput;
 @property (nonatomic, strong) CAGradientLayer * gradientLayer;
 
+// Store previous layer
+@property (nonatomic, strong) CAGradientLayer *redLayer;
+@property (nonatomic, strong) CAGradientLayer *greenLayer;
+@property (nonatomic, strong) CAGradientLayer *blueLayer;
+
 @end
 
 @implementation PCLViewController
@@ -36,8 +41,8 @@
 
 - (void)updateBox:(UIView *)box startR:(int)startR startG:(int)startG startB:(int)startB finishR:(int)finishR finishG:(int)finishG finishB:(int)finishB
 {
-
     
+    // Initial setup is the same for the three boxes
     self.gradientLayer = [CAGradientLayer layer];
     self.gradientLayer.frame = box.bounds;
     self.gradientLayer.startPoint = CGPointMake(0, 0);
@@ -53,8 +58,41 @@
                                           alpha:1.0];
 
     self.gradientLayer.colors = [NSArray arrayWithObjects: (id)[startColor CGColor],(id)[finishColor CGColor], nil];
-                                 
-    [box.layer insertSublayer:self.gradientLayer atIndex:1];
+    
+    // Check to see if a layer already exists
+    bool previousLayer = false;
+    if ( [box tag] == 1 )
+    {
+        if (self.redLayer != nil ) // box is red
+        {
+            [box.layer replaceSublayer:self.redLayer with:self.gradientLayer];
+            previousLayer = true;
+        }
+        [self setRedLayer:self.gradientLayer];
+    }
+    else if ( box.tag == 2 )
+    {
+        if ( self.greenLayer != nil ) // box is green
+        {
+            [box.layer replaceSublayer:self.greenLayer with:self.gradientLayer];
+            previousLayer = true;
+        }
+        [self setGreenLayer:self.gradientLayer];
+    }
+    else if ( [box tag] == 3 )
+    {
+        if ( self.blueLayer != nil ) // box is blue
+        {
+            [box.layer replaceSublayer:self.blueLayer with:self.gradientLayer];
+            previousLayer = true;
+        }
+        [self setBlueLayer:self.gradientLayer];
+    }
+
+    if ( ! previousLayer)
+    {
+        [box.layer addSublayer:self.gradientLayer];
+    }
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -78,11 +116,14 @@
     
 
     
+    // Get the initial gradients in each box
     [self updateBox:self.redView startR:0 startG:0 startB:0 finishR:1 finishG:0 finishB:0];
+    [self updateBox:self.greenView startR:0 startG:0 startB:0 finishR:0 finishG:1 finishB:0];
+    [self updateBox:self.blueView startR:0 startG:0 startB:0 finishR:0 finishG:0 finishB:1];
     
+[self updateBox:self.redView startR:0 startG:0 startB:0 finishR:1 finishG:0 finishB:1];
     
-    NSLog(@"Success");
-    // Do any additional setup after loading the view from its nib.
+    self.redView.backgroundColor = [UIColor purpleColor];
 }
 
 - (void)didReceiveMemoryWarning
