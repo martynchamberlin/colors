e090030a43d4e8c9c263604286d4fbb48b4c7139
Martyn Chamberlin <martynchamberlin@martyns-mbp.gateway.2wire.net>
2014-08-07 21:47:03 -0400

    Making some serious rebooting here

---
+++ ColorApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ColorApp
+//
+//  Created by Martyn Chamberlin on 8/7/14.
+//  Copyright (c) 2014 Martyn Chamberlin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ColorAppTests/ColorAppTests.m
@@ -0,0 +1,40 @@
+//
+//  ColorAppTests.m
+//  ColorAppTests
+//
+//  Created by Martyn Chamberlin on 8/7/14.
+//  Copyright (c) 2014 Martyn Chamberlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ColorAppTests : XCTestCase
+
+@end
+
+@implementation ColorAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
