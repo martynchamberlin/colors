7b25b7f1deedb9bb92dd84dedad5276cb3693656
Martyn Chamberlin <martynchamberlin@martyns-mbp.gateway.2wire.net>
2014-08-08 01:22:42 -0400

    That took way too long

--- SliderView.m
+++ SliderView.m
@@ -12,6 +12,10 @@
 
 @property (nonatomic) int xBeforeTouchesMoved;
 
+@property (nonatomic, strong) IBOutlet UIView * sliderControlRView;
+@property (nonatomic, strong) IBOutlet UIView * sliderControlGView;
+@property (nonatomic, strong) IBOutlet UIView * sliderControlBView;
+
 @end
 
 @implementation SliderView
@@ -29,18 +33,32 @@
     UITouch *anyTouch = [touches anyObject];
     CGPoint touchLocation = [anyTouch locationInView:self.superview];
     float distanceMoved = touchLocation.x - self.xBeforeTouchesMoved;
-    CGRect rect = self.frame;
+    CGRect rect = [[self getThing:self.tag] frame];
     rect.origin.x += distanceMoved;
-    
+    NSLog( @"%f", distanceMoved);
     // As long as they aren't trying to scroll out of their screen allow the Slider to scroll along x axis
-    if ( rect.origin.x >= 0 && rect.origin.x + self.frame.size.width <= self.superview.frame.size.width )
-    {
-        self.frame = rect;
+    if ( rect.origin.x >= 0 && rect.origin.x + rect.size.width <= self.superview.frame.size.width ) {
+        [[self getThing:self.tag] setFrame: rect];
+        NSLog(@"yes");
     }
     
     self.xBeforeTouchesMoved = touchLocation.x;
 }
 
-
+// Man, this is redneck
+-(UIView *)getThing:(NSUInteger)tag{
+    if ( tag == 0 )
+    {
+        return self.sliderControlRView;
+    }
+    else if ( tag == 1 )
+    {
+        return self.sliderControlGView;
+    }
+    else // if ( tag == 2)
+    {
+        return self.sliderControlBView;
+    }
+}
 
 @end
